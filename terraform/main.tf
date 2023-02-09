a88cfcd7d15ae0a27a321d9082338dcf0c08e9b1
Idan Horovitz <idanhh55@gmail.com>
2023-02-09 13:19:11 +0200

    Add variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,88 +1,112 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Define the AWS provider.
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region     = "us-east-1"
+  region     = var.region
 }
 
+# Create an S3 bucket.
 resource "aws_s3_bucket" "bucket_idanho" {
-  bucket = "bucket-idanho55"
+  bucket = var.bucket_name
 }
 
+# Data source for the lambda code archive
 data "archive_file" "code_archive" {
-  type        = "zip"
-  source_dir  = "../src"
-  output_path = "../terraform/lambda_handler.zip"
+  type        = "zip"  # Type of archive file
+  source_dir  = "../src"  # Directory containing the source code
+  output_path = "../terraform/${var.lambda_handler_zip_file}"  # Path to store the output archive
 }
 
+# S3 object for uploading the lambda code archive
 resource "aws_s3_object" "code_object" {
-  bucket = aws_s3_bucket.bucket_idanho.bucket
-  key    = "lambda_handler.zip"
-  source = data.archive_file.code_archive.output_path
+  bucket = aws_s3_bucket.bucket_idanho.bucket  # S3 bucket name
+  key    = var.lambda_handler_zip_file  # Key for the object
+  source = data.archive_file.code_archive.output_path  # Source of the object
 }
 
+# IAM role for the lambda function
 resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda_execution_role"
+  name = var.lambda_execution_role_name  # Name of the IAM role
 
-  assume_role_policy = jsonencode({
+  assume_role_policy = jsonencode({  # Policy for the IAM role
     Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
+        Action = "sts:AssumeRole",  # Action for the role
+        Effect = "Allow",  # Allow access
         Principal = {
-          Service = "lambda.amazonaws.com"
+          Service = "lambda.amazonaws.com"  # Principal for the role
         }
       }
     ]
   })
 }
 
+# Create the lambda function
 resource "aws_lambda_function" "lambda_function" {
-  function_name = "lambda_handler"
+  function_name = var.lambda_function_name  # Name of the lambda function
 
-  s3_bucket = aws_s3_bucket.bucket_idanho.bucket
-  s3_key    = aws_s3_object.code_object.key
-  role      = aws_iam_role.lambda_execution_role.arn
-  handler   = "lambda_function.lambda_handler"
-  runtime   = "python3.9"
+  s3_bucket = aws_s3_bucket.bucket_idanho.bucket  # S3 bucket containing the code archive
+  s3_key    = aws_s3_object.code_object.key  # Key for the code archive object
+  role      = aws_iam_role.lambda_execution_role.arn  # IAM role for the lambda function
+  handler   = var.lambda_handler_file  # Entry point for the lambda function
+  runtime   = var.python_runtime  # Python runtime for the lambda function
 }
 
+# Create the API Gateway
 resource "aws_apigatewayv2_api" "api_gateway" {
-  name          = "serverless_lambda_gw"
-  protocol_type = "HTTP"
+  name          = var.api_gateway_name  # Name of the API Gateway
+  protocol_type = var.api_gateway_protocol_type  # Protocol type for the API Gateway
 }
 
+# Integrate the API Gateway with the lambda function
 resource "aws_apigatewayv2_integration" "api_gateway_integration" {
-  api_id = aws_apigatewayv2_api.api_gateway.id
+  api_id = aws_apigatewayv2_api.api_gateway.id  # ID of the API Gateway
 
-  integration_uri    = aws_lambda_function.lambda_function.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
+  integration_uri    = aws_lambda_function.lambda_function.invoke_arn  # Invoke ARN of the lambda function
+  integration_type   = var.api_gateway_integration_type  # Integration type for the API Gateway
+  integration_method = var.api_gateway_integration_method  # Integration method for the API Gateway
 }
 
+# Create a stage for the API Gateway
 resource "aws_apigatewayv2_stage" "api_gateway_stage" {
-  api_id = aws_apigatewayv2_api.api_gateway.id
+  api_id = aws_apigatewayv2_api.api_gateway.id  # ID of the API Gateway
 
-  name        = "serverless_lambda_stage"
-  auto_deploy = true
+  name        = var.api_gateway_stage_name  # Name of the API Gateway stage
+  auto_deploy = true  # Auto-deploy the API Gateway stage
 }
 
+# Create a route for the API Gateway.
 resource "aws_apigatewayv2_route" "api_gateway_route" {
+  # ID of the API Gateway
   api_id = aws_apigatewayv2_api.api_gateway.id
 
-  route_key = "GET /"
-  target    = "integrations/${aws_apigatewayv2_integration.api_gateway_integration.id}"
+  # Key of the route
+  route_key = var.api_gateway_route_key
+
+  # Target of the route, integration ID of the API Gateway
+  target = "integrations/${aws_apigatewayv2_integration.api_gateway_integration.id}"
 }
 
+# Grant permission to the API Gateway to call the Lambda function.
 resource "aws_lambda_permission" "api_gateway_permissions" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_function.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
+  statement_id  = var.api_gateway_permissions_statement_id # Unique identifier for the permission
+  action        = var.api_gateway_permissions_action # Action of the permission
+  function_name = aws_lambda_function.lambda_function.function_name # Name of the Lambda function
+  principal     = var.api_gateway_permissions_principal # Principal of the permission
+  source_arn = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*" # ARN of the source to grant permission to
 }
 
+# Output the URL of the API Gateway stage
 output "api_gateway_url" {
   value = "${aws_apigatewayv2_stage.api_gateway_stage.invoke_url}/"
 }
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,7 +1,122 @@
 variable "secret_key" {
-  type = string
+  type    = string
+
+  description = "The AWS secret key for accessing the AWS resources."
 }
 
 variable "access_key" {
-  type = string
+  type    = string
+
+  description = "The AWS access key for accessing the AWS resources."
 }
+
+variable "region" {
+  type    = string
+
+  description = "The AWS region where the resources will be created."
+}
+
+
+variable "bucket_name" {
+  type    = string
+
+  description = "The name of the S3 bucket that will store the AWS Lambda code."
+}
+
+variable "lambda_handler_zip_file" {
+  type    = string
+  default = "lambda_handler.zip"
+
+  description = "The name of the zip file containing the AWS Lambda function code."
+}
+
+variable "lambda_execution_role_name" {
+  type    = string
+  default = "lambda_execution_role"
+
+  description = "The name of the AWS IAM role for the AWS Lambda function."
+}
+
+variable "lambda_function_name" {
+  type    = string
+  default = "lambda_handler"
+
+  description = "The name of the AWS Lambda function."
+}
+
+variable "lambda_handler_file" {
+  type    = string
+  default = "lambda_function.lambda_handler"
+
+  description = "The file name and entry point of the AWS Lambda function code."
+}
+
+variable "python_runtime" {
+  type    = string
+  default = "python3.9"
+
+  description = "The version of Python runtime for the AWS Lambda function."
+}
+
+variable "api_gateway_name" {
+  type    = string
+  default = "lambda_gateway"
+
+  description = "The name of the API Gateway that will route incoming API requests to the AWS Lambda function."
+}
+
+variable "api_gateway_protocol_type" {
+  type    = string
+  default = "HTTP"
+
+  description = "The protocol used by the API Gateway for incoming API requests."
+}
+
+variable "api_gateway_integration_type" {
+  type    = string
+  default = "AWS_PROXY"
+
+  description = "The type of integration used by the API Gateway to route incoming API requests to the AWS Lambda function."
+}
+
+variable "api_gateway_integration_method" {
+  type    = string
+  default = "POST"
+
+  description = "The HTTP method used by the API Gateway for incoming API requests."
+}
+
+variable "api_gateway_stage_name" {
+  type    = string
+  default = "lambda_stage"
+
+  description = "The name of the stage for the API Gateway."
+}
+
+variable "api_gateway_route_key" {
+  type    = string
+  default = "GET /"
+
+  description = "The route key for incoming API requests in the API Gateway."
+}
+
+variable "api_gateway_permissions_statement_id" {
+  type    = string
+  default = "AllowExecutionFromAPIGateway"
+
+  description = "The identifier of the AWS IAM policy statement that allows the API Gateway to invoke the AWS Lambda function."
+}
+
+variable "api_gateway_permissions_action" {
+  type    = string
+  default = "lambda:InvokeFunction"
+  description = "The action that the API Gateway is authorized to perform on the Lambda function."
+}
+
+variable "api_gateway_permissions_principal" {
+  type    = string
+  default = "apigateway.amazonaws.com"
+  description = "The AWS service that the API Gateway belongs to and that is authorized to perform the action on the Lambda function."
+}
+
+
